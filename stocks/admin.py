595a0855612fd058beba802e327ac02489bf72bb
anf692 <fassane348@gmail.com>
2026-07-04 21:30:09 +0000

    Ajoute des modeles dans l'admin

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Warehouse, Product
 # Register your models here.
+
+
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nom', 'localisation', 'capacite')
+    search_fields = ('nom', 'localisation')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nom', 'quantite', 'date_expiration', 'etat', 'entrepot')
+    search_fields = ('nom', 'entrepot__nom')
+
